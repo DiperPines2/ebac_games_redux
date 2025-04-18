230a97f0029d37df53a74eeeaf4a9f5c2f7ec7aa
Wanderx13 <pupetfuntasy43@gmail.com>
2025-04-18 11:28:40 -0300

    f2

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import carrinhoReducer from './reducers/Carrinho'
+
+export const store = configureStore({
+  reducer: {
+    carrinho: carrinhoReducer
+  }
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
---
+++ src/store/reducers/Carrinho.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Game } from '../../App'
+
+type CarrinhoState = {
+  itens: Game[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Game>) => {
+      const jogo = action.payload
+
+      if (state.itens.find((game) => game.id === jogo.id)) {
+        alert('Item Já adicionado')
+      } else {
+        state.itens.push(jogo)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrrinhoSlice.actions
+export default carrrinhoSlice.reducer
